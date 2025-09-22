a5099fd0a8b9455969138998b4a9a0c4a3014d32
WonJin <try3982@kakao.com>
2025-09-22 18:54:31 +0900

    -. 상품 폴더 설계

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
